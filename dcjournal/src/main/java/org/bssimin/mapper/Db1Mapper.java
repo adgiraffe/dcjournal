773370ee5082091211b6e0a1c48f4ada7ae9006d
joo <ozzznet@naver.com>
2017-07-16 19:02:04 +0900

    이미지 객체 추가 버젼 db테이블에 이미지경로 리스트 추가 된다

--- dcjournal/src/main/java/org/bssimin/mapper/Db1Mapper.java
+++ dcjournal/src/main/java/org/bssimin/mapper/Db1Mapper.java
@@ -3,7 +3,7 @@ package org.bssimin.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.bssimin.domain.GeUser_info;
-import org.springframework.stereotype.Repository;
+import org.bssimin.domain.ImageVO.Image_info;
 
 import java.util.List;
 
@@ -16,8 +16,6 @@ import java.util.List;
 public interface Db1Mapper {
     public GeUser_info readGeUser(String geUserId);
     public List<String> readAuthority(String geUserPw);
-
-
-
+//    public void regiImage(Image_info info);
 
 }
